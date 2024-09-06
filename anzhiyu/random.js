77b88201b2552f02f7a520221cb61ecae0b8b953
smallstar666 <2065400281@qq.com>
2024-09-06 09:07:01 +0800

    Site updated: 2024-09-06 09:06:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/29/hello-world/"];function toRandomPost(){
+var posts=["2024/09/06/clion_cubemx/","2024/08/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
